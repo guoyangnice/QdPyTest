885365683242bf583d02b6087bf4cb087425503c
guoyangnice <yangjienice@126.com>
2021-04-09 10:43:45 +0800

    yj@feat:完成测试开发框架demo

--- demo.py
+++ demo.py
@@ -1,15 +1,13 @@
+# filename: test_reg.py
 import requests
+import urllib
 
-
-def test_user_reg():
+def test_user_reg():  # 可以不用写类
     url = "http://www.baidu.com"
-    # 部分网站需要代理
     # headers = {'User-Agent': 'User-Agent:Mozilla/5.0'}
     # data1 = urllib.request.Request(url, headers=headers)
     # data = urllib.request.urlopen(data1).read()
     res = requests.post(url)
-    assert 302 == res.status_code
-
-
+    assert 302 == res.status_code  # 断言使用Python原生assert
 if __name__ == '__main__':
     test_user_reg()
